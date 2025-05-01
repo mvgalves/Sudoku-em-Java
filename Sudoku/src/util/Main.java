7e6d4469fe4f1187a08844273bb6fffb4601e63c
mvgalves <markitusalves2@gmail.com>
2025-05-01 11:39:50 -0300

    Sudoku pelo console completo

---
+++ Sudoku/src/util/Main.java
@@ -0,0 +1,19 @@
+package util;
+import java.util.Scanner;
+import java.util.stream.Stream;
+public class Main {
+   
+    private final static Scanner scanner = new Scanner(System.in);
+
+    private static Board board;
+
+    private final static int BOARD_LIMIT + 9;
+    
+    
+    
+    public static void main(String[] args) {
+        final var positions : Stream<String> = Stream.of(args)
+        
+    }
+    
+}
